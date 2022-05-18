d9731a6df9e20211614186918b7b0d8346d15a2c
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2022-05-18 17:02:23 +0200

    feat(styles) : ajout de styles à Tweet

--- src/components/tweet/Tweet.jsx
+++ src/components/tweet/Tweet.jsx
@@ -8,21 +8,23 @@ import './tweet.scss';
 const Tweet = ({ tweet, user}) => {
   return (
     <div className='tweet'>
-        <div>
-        <Link to={`/users/${tweet.authorId}`} state={{tweet: tweet}}>
-          <div className='tweetHeader'>
-            <span>hey</span>
-            <h2>@{tweet.author}</h2>
-          </div>
-          </Link>
-          <p>{tweet.message}</p>
-          <div className="icons">
-            <Link to={`/tweets/${tweet.id}`} state={{tweet: tweet}}><FaRegComment />{tweet.comments ? (tweet.comments.length) : "0"}</Link>
-            <AiOutlineRetweet />
-            <AiOutlineHeart />
-            <IoShareOutline />
-          </div>
+      <div className='pictureContainer'>
+        <img src="https://thispersondoesnotexist.com/image" alt="" />
+      </div>
+      <div className='tweetContainer'>
+      <Link to={`/users/${tweet.authorId}`} state={{tweet: tweet}}>
+        <div className='tweetHeader'>
+          <h2>@{tweet.author}</h2>
         </div>
+        </Link>
+        <p>{tweet.message}</p>
+        <div className="icons">
+          <Link to={`/tweets/${tweet.id}`} state={{tweet: tweet}}><FaRegComment />{tweet.comments ? (tweet.comments.length) : "0"}</Link>
+          <AiOutlineRetweet />
+          <AiOutlineHeart />
+          <IoShareOutline />
+        </div>
+      </div>
     </div>
   );
 };
